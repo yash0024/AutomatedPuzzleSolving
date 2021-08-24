9f8407e799a8400dc4b08e78044572e80d17ddcc
yash0024 <yash.ramani@mail.utoronto.ca>
2021-08-24 21:52:40 +0300

    Update adts.py

--- adts.py
+++ adts.py
@@ -1,20 +1,8 @@
 """
-One implementation each of Stack and Queue.
-
-This code is provided solely for the personal and private use of students
-taking the CSC148 course at the University of Toronto. Copying for purposes
-other than this use is expressly prohibited. All forms of distribution of
-this code, whether as given or with any changes, are expressly prohibited.
-
-This file is:
-Copyright (c) 2021 Diane Horton, Jonathan Calver, Sophia Huynh, Maryam Majedi,
-and Jaisie Sin.
-
 ===== Module Description =====
 
 This module contains list-based implementations of the Stack and Queue
-ADTs. You are NOT required to use either of these in this assignment,
-but we include them for those who wish to do so.
+ADTs.
 """
 from typing import List, Optional, Any
 
